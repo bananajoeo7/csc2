f6490c8c1a52e20d7e5a2313b1e9801fa928b4a1
Trostin Geles <trostinrocks@icloud.com>
2024-12-06 02:35:54 -0500

    test #2

--- C/Programs/Word_Per_Line_EX_1-10.c
+++ C/Programs/Word_Per_Line_EX_1-10.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+printf("Print a sentence that you would like printed out line by line:\n");
+
+
 main (argc, argv)
 int argc;
 char **argv;
 {
         int i;
-
-        printf("Print a sentence that you would like printed out line by line:\n");
 
         for (i = 1; i < argc; i++)
 {               printf("%s\n", argv[i]);
