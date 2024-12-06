4cf738485f9510249d4d622a639aac02aeb82375
Trostin Geles <trostinrocks@icloud.com>
2024-12-06 02:41:10 -0500

    exercise 10 complete

--- C/Programs/Word_Per_Line_EX_1-10.c
+++ C/Programs/Word_Per_Line_EX_1-10.c
@@ -5,6 +5,8 @@ int argc;
 char **argv;
 {
         int i;
+        
+        printf("Here is your sentence with one word on every line: \n");
 
         for (i = 1; i < argc; i++)
 {               printf("%s\n", argv[i]);
