59aabb03637c1a4dcd8f4b0a4b711ebb1de26a04
Trostin Geles <trostinrocks@icloud.com>
2024-12-11 13:51:19 -0500

    Finished for now, I believe there might be an issue with the version of C we are using

--- C/Programs/DoY_and_DoM.c
+++ C/Programs/DoY_and_DoM.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int *day_tab[13];
-    day_tab[0] = 0;
-    day_tab[1] = 31;
-    day_tab[2] = 28;
-    day_tab[3] = 31;
-    day_tab[4] = 30;
-    day_tab[5] = 31;
-    day_tab[6] = 30;
-    day_tab[7] = 31;
-    day_tab[8] = 31;
-    day_tab[9] = 30;
-    day_tab[10] = 31;
-    day_tab[11] = 30;
-    day_tab[12] = 31;
+day_tab[0] = 0;
+day_tab[1] = 31;
+day_tab[2] = 28;
+day_tab[3] = 31;
+day_tab[4] = 30;
+day_tab[5] = 31;
+day_tab[6] = 30;
+day_tab[7] = 31;
+day_tab[8] = 31;
+day_tab[9] = 30;
+day_tab[10] = 31;
+day_tab[11] = 30;
+day_tab[12] = 31;
 
 day_of_year (year, month, day) /*set day of year*/
 int year, month, day; /*from month of day*/
@@ -28,6 +28,23 @@ int year, month, day; /*from month of day*/
         }
     }
     return(day);
+}
+
+month-day(year, yearday, pmonth, pday) /*set month, day*/
+int year, yearday, *pmonth, *pday; /*from day of year*/
+{
+  int i;
+
+  for (i = 1; yearday > day_tab + i; i++) {
+    if(month == 2 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            yearday -= day_tab + i;
+        } else {
+            yearday -= day_tab + i + 1;
+        }
+    
+  }
+  *pmonth = i;
+  *pday = yearday;
 }
 
 main() {
