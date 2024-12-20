894423ba9f948eee1805ee52f0a67a6ed2855ae0
Trostin Geles <trostinrocks@icloud.com>
2024-12-20 08:44:01 -0500

    unfinished attemps to split string

--- BigInt/Programs/testcalc.c
+++ BigInt/Programs/testcalc.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
-/*
-This is a test program for bigint struct.
-*/
 
 struct bigint {
     char negative;
     char numdigits;
-    char* digits;
+    char digits[10];
 };
 
-int main()
-{
-    
+int main() {
+    char input[100];
+    printf("Enter a string of digits: ");
+    scanf("%99s", input);
 
+    int input_len;
+    input_len = 0;
+    while (input[input_len] != '\0') {
+        input_len++;
+    }
+
+    printf("Chunks of 9 digits:\n");
+
+    for (int i = 0; i < input_len;) {
+        i += 9;
+
+        struct bigint chunk;
+        chunk.negative = 0;
+        chunk.numdigits = 0;
+
+        int j;
+        for (j = 0; j < 9 && (i + j) < input_len; j++) {
+            chunk.digits[j] = input[i + j];
+        }
+        chunk.digits[j] = '\0';
+        chunk.numdigits = j;
+
+        printf("Chunk %d: %s\n", (i/9) + 1, chunk.digits);
+    }
+
+    return 0;
 }
-
