f3a427885e98b8a4ecb13a240fd5d975726fe28f
Trostin Geles <trostinrocks@icloud.com>
2025-01-12 23:30:31 -0500

    BigInt Update

--- BigInt/Programs/BigIntTD.c
+++ BigInt/Programs/BigIntTD.c
@@ -4,7 +4,8 @@
 main() {
     START_TESTING("BIGINTTD.C");
 
-    TEST_CASE("Read and write bigint 1234567") {
+    TEST_CASE("Read and write bigint 1234567");
+    {
         struct bigint bi;
         set_bigint("1234567", &bi);
         ASSERT_STR(get_bigint(bi, "1234567"));
